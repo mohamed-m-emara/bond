803d3abd59eb2fbaeeb0fcd25125c1ca3aa2b7f7
Mohamed Mohsen Emara <mohamed.mohsen1701@gmail.com>
2024-05-12 23:22:08 +0300

    Taha's part
    my part included doing things in the donor dashboard.

--- donorFulfillDonations.js
+++ donorFulfillDonations.js
@@ -24,29 +24,18 @@ document.getElementById('notification-icon').addEventListener('click', function 
   };
 
 
-  // Get the elements
-// Get the elements
-var input = document.querySelector('.search-form input');
-var cards = document.querySelectorAll('.profile-card');
 
-// Add an event listener for the input field
-input.addEventListener('keyup', function (event) {
-  // Get the search query
-  var query = event.target.value.toLowerCase();
 
-  // Loop through all the profile cards
-  cards.forEach(function (card) {
-    var organization = card.querySelector('a').textContent;
+let quantity = 0;
 
-    // If the organization includes the search query, show it, otherwise hide it
-    if (organization.toLowerCase().includes(query)) {
-      card.style.display = '';
-    } else {
-      card.style.display = 'none';
-      
+function increaseQuantity() {
+    quantity++;
+    document.getElementById('quantity').textContent = quantity;
+}
+
+function decreaseQuantity() {
+    if (quantity > 0) {
+        quantity--;
+        document.getElementById('quantity').textContent = quantity;
     }
-  });
-});
-
-
-
+}
